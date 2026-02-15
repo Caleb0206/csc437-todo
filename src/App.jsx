4066a3a6e7aaf9907d94287d5352a5c3b9c1ed3f
calebhuang <calebhuang0206@gmail.com>
2026-02-14 17:22:55 -0800

    unique ids using nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import { nanoid } from "nanoid";
 import { TodoItem } from './TodoItem';
 import { AddTaskForm } from './AddTaskForm';
-import './App.css'
+import './App.css';
 
 const MY_INITIAL_TASK_LIST = [
-  { id: "todo-0", name: "Eat", isComplete: true },
+  { id: "todo-0", name: "Eat", isComplete: false },
   { id: "todo-1", name: "Sleep", isComplete: false },
   { id: "todo-2", name: "Repeat", isComplete: false }
 ]
 
 function App() {
   const [taskList, setTaskList] = useState(MY_INITIAL_TASK_LIST);
+
+  function addTask() {
+    const newTask = { id: nanoid(), name: "New Task", isComplete: false };
+    const taskListClone = [...taskList, newTask];
+    setTaskList(taskListClone);
+  }
 
   return (
     <main className="m-4"> {/* Reminder: React code uses className instead of class */}
@@ -30,7 +36,8 @@ function App() {
             />
           ))}
         </ul>
-        
+        <button onClick={addTask} className="p-1 bg-blue-500 text-white">New But Can't Set Name</button>
+
       </section>
     </main>
   );
